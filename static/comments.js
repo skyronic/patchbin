9fa0dafc7d85edd82162832c67d6100c70dc2ab0
Anirudh Sanjeev <andy@ninjagod.com>
2010-01-12 15:59:37 +0530

    More clientside commenting functionality

--- static/comments.js
+++ static/comments.js
@@ -6,7 +6,57 @@ function main()
 	$("td.whiteback").bind("dblclick", drawCommentForm);
 	$("td.redback").bind("dblclick", drawCommentForm);
 	$("td.greenback").bind("dblclick", drawCommentForm);
+	addCommentToDiffLine('lhs', '2', '75', '1', 'Anirudh', "Hey There!");
+}
 
+function appendDivToDiffLine(id, contentDiv)
+{
+	// Do we need to do anything else?
+	$("#" + id).append(contentDiv);
+}
+
+/**
+ * Do not call. Triggered by jQuery
+ */
+function replyToComment(e)
+{
+
+}
+
+function addCommentToDiffLine(side, chunkNum, line, commentIndex, author, content)
+{
+	idString = diffLineIdFromParams(side, chunkNum, line);
+	if(idString != "error")
+	{
+		commentDiv = document.createElement("div");
+		commentDiv.innerHTML = '<div class="commentText"><p class="commentAuthor">'
+			+ author + ' Said:</p><p class="commentContent">' + content +
+			'</p><p>[<a>Reply</a>]</p></div>';
+
+		appendDivToDiffLine(idString, commentDiv);
+		$(commentDiv).find("a").bind("click", {commentElement:commentDiv}, replyToComment);
+	}
+}
+
+function diffLineIdFromParams(side, chunkNum, line)
+{
+	idString = side + "-" + chunkNum + "-" + line;
+	if(document.getElementById(idString))
+		return idString;
+	return "error";
+}
+function postCommentFromForm(e)
+{
+	// get the container div
+	if(typeof(e.data.commentDiv != "undefined"))
+	{
+		commentDiv = e.data.commentDiv;
+		commentString = $(commentDiv).find('textarea')[0].value;
+		commentName = $(commentDiv).find('input')[0].value;
+
+		console.log(commentName + " wrote - " + commentString);
+		this.disabled = true;
+	}
 }
 
 function drawCommentForm (e)
@@ -16,7 +66,24 @@ function drawCommentForm (e)
 	console.log(this);
 	
 
-	$(this.parentNode).after("<tr><th></th><td>hi there</td><th></th><td></td></tr>");
+	//$(this.parentNode).after("<tr><th></th><td>hi there</td><th></th><td></td></tr>");
+	commentFormDiv = document.createElement("div");
+	commentID = 0;
+	commentFormDiv.id = this.id + "-comment-" + commentID;
+	commentFormDiv.innerHTML = '<div id="" class=""><p>Name: <input type="text" id="" /></p><p><textarea></textarea></p><p><button>Submit!</button></div>';
+	$(commentFormDiv).addClass("commentform");
+
+	if($(commentFormDiv).hasClass("whiteback"))
+		$(commentFormDiv).removeClass("whiteback");
+	if($(commentFormDiv).hasClass("greenback"))
+		$(commentFormDiv).removeClass("greenback");
+	if($(commentFormDiv).hasClass("redback"))
+		$(commentFormDiv).removeClass("redback");
+
+	appendDivToDiffLine(this.id, commentFormDiv);
+
+	// Find the submit button and hook to the click event
+	$(this).find('button').bind('click', {commentDiv:commentFormDiv}, postCommentFromForm);
 }
 
 $(document).ready(main);
