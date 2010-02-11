cd86931bc25934aea6673fd22b7d262b1d887e18
Anirudh Sanjeev <andy@ninjagod.com>
2010-02-11 15:23:27 +0530

    More improvements to patchbin upload script

--- tools/patchbin.py
+++ tools/patchbin.py
@@ -1,11 +1,30 @@
 #!/usr/bin/python
+# Copyright (c) 2010 Anirudh Sanjeev <anirudh@anirudhsanjeev.org>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 import sys
 import urllib2, urllib
 
-print "Send-to-patchbin tool. Usage examples: 'svn diff | sendtopatchbin'"
-print "Listening on stdin..."
+print "Patchbin upload script. Usage examples: 'diff file1 file2|patchbin', 'git\
+diff | patchbin', etc."
+print "Listening on stdin... ^C to quit"
 input_stdin = sys.stdin.read()
-print "input is", input_stdin
 
 name = ""
 description = "Uploaded via patchbin upload script"
@@ -20,11 +39,11 @@ if __name__ == "__main__":
 			'authorEmail':email,
 			'patchDesc':description
 			}
-	data = urllib.urlencode(postdata)
-	req = urllib2.Request(url, data)
-	response = urllib2.urlopen(req)
-
 	try:
+		data = urllib.urlencode(postdata)
+		req = urllib2.Request(url, data)
+		response = urllib2.urlopen(req)
+
 		if(response.url != url):
 			# Completed successfully
 			print "SUCCESS!! View your patch online at:\n\n" + response.url + "\n\n"
@@ -35,5 +54,5 @@ if __name__ == "__main__":
 			print response.read()
 	except Exception, e:
 		print "Unable to make the HTTP Request."
-		print "Error details: ", e.message
+		print "Error details: ", e
 
